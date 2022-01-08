b7d6029f897f423cbc8ece43c1b4f09fd292ca17
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-08 18:29:58 +0330

    addEventListener added

--- 1/1.js
+++ 1/1.js
@@ -1,17 +1,14 @@
 const tile=document.getElementsByClassName("tile")
 const tileArr=Array.from(tile)
-const fillX=tileArr.children
-const fillO=tileArr.children
-console.log(fillX)
-// let counter=0
-// tileArr.map('click',e=>{
-//     counter++
-//     if(counter%2==0){
-//         e.style.backgroundColor="red"
-//     }else{
-//         fillO.style.display="block" 
-//     }
+let counter=0
+tileArr.map(input=>{
+input.addEventListener('click',e=>{
+        counter++
+        if(counter%2==0){
+            input.classList.add("fill-o") 
+        }                                                                                                                                                                   
+   
+})
+})
+  
 
-
-
-// })
