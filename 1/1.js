16b8970aab8faab9729f17e3b14b2d5a1438a5e3
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-08 13:24:42 +0330

    files created

---
+++ 1/1.js
@@ -0,0 +1,17 @@
+const tile=document.getElementsByClassName("tile")
+const tileArr=Array.from(tile)
+const fillX=tileArr.children
+const fillO=tileArr.children
+console.log(fillX)
+// let counter=0
+// tileArr.map('click',e=>{
+//     counter++
+//     if(counter%2==0){
+//         e.style.backgroundColor="red"
+//     }else{
+//         fillO.style.display="block" 
+//     }
+
+
+
+// })
