88e7c6c8a68610de94d6790772e79a7dc3370e36
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-10 20:21:01 +0330

    bootstrap added

--- 1/1.js
+++ 1/1.js
@@ -1,14 +1,22 @@
+
 const tile=document.getElementsByClassName("tile")
 const tileArr=Array.from(tile)
 let counter=0
-tileArr.map(input=>{
+let x=0
+let o=0
+tileArr.map((input,i)=>{
 input.addEventListener('click',e=>{
         counter++
         if(counter%2==0){
-            input.classList.add("fill-o") 
-        }                                                                                                                                                                   
-   
+            input.classList.add("fill-o")
+        }else{
+            input.classList.add("fill-x")
+        }
+        
+    },{once : true})
 })
-})
-  
+
+
+
+
 
